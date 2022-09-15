aa4f4f71fc416e4ce647d36f133e23bcbb89e94e
Bisquitman <lynx8309@gmail.com>
2022-09-15 23:59:40 +0300

    Day 3

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,26 @@
-import {sum} from './modules/test.js';
+import Swiper from '../assets/libs/swiper/swiper-bundle.esm.browser.min.js';
 
-document.write(sum(3, 5));
+new Swiper('.goods__block', {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 24,
+    },
+    1440: {
+      slidesPerView: 3,
+      spaceBetween: 24,
+    },
+  },
+  navigation: {
+    prevEl: '.goods__arrow_prev',
+    nextEl: '.goods__arrow_next',
+  },
+});
